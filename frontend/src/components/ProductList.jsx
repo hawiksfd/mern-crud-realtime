4ea871e98a46b46bb31278dff6eec3ea69f6a22b
hawik <hawiksyaef@gmail.com>
2022-08-25 22:38:01 +0700

    membuat tombol add new, edit dan delete dg Link

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const ProductList = () => {
   return (
     <div className="flex flex-col mt-5">
       <div className="w-full">
+
+        <Link to="/add"  
+            className="
+            bg-green-400 hover:bg-green-700 border-slate-200
+            text-white font-bold py-2 px-4 rounded-lg"
+            >Add New</Link>
+
         <div className="relative shadow rounded-lg mt-3">
             <table className="w-full text-sm text-left text-gray-500">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-100">
@@ -19,7 +27,14 @@ const ProductList = () => {
                         <td className="py-3 px-1 text-center"></td>
                         <td className="py-3 px-6 font-medium text-gray-900">Product 1</td>
                         <td className="py-3 px-6">2456</td>
-                        <td className="py-3 px-1 text-center"></td>
+                        <td className="py-3 px-1 text-center">
+                          <Link to={`/edit/`}
+                            className="font-medium bg-blue-400 hover:bg-blue-600 
+                            text-white py-1 px-4 rounded mr-1">Edit</Link>
+                            <button to="/edit"  
+                            className="font-medium bg-red-400 hover:bg-red-600 
+                            text-white py-1 px-4 rounded">delete</button>
+                        </td>
                     </tr>
                 </tbody>
             </table>
