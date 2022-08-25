c6c435842de1305a11258e3247685aec578086c3
hawik <hawiksyaef@gmail.com>
2022-08-25 17:00:03 +0700

    konfigurasi product controller

--- backend/controllers/ProductControl.js
+++ backend/controllers/ProductControl.js
@@ -1,9 +1,73 @@
-export const getProducts = (req, res) => {};
+import { PrismaClient } from "@prisma/client";
 
-export const getProductById = (req, res) => {};
+const prisma = new PrismaClient();
 
-export const createProduct = (req, res) => {};
+export const getProducts = async (req, res) => {
+  try {
+    const response = await prisma.product.findMany();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
 
-export const updateProduct = (req, res) => {};
+export const getProductById = async (req, res) => {
+  try {
+    const response = await prisma.product.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(404).json({ msg: error.message });
+  }
+};
 
-export const deleteProduct = (req, res) => {};
+export const createProduct = async (req, res) => {
+  const { name, price } = req.body;
+
+  try {
+    const product = await prisma.product.create({
+      data: {
+        name: name,
+        price: price,
+      },
+    });
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { name, price } = req.body;
+
+  try {
+    const product = await prisma.product.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        name: name,
+        price: price,
+      },
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await prisma.product.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
