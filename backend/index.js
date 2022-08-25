90a2c3dbdb21ee66e7a7a9ee78171537a2d09487
hawik <hawiksyaef@gmail.com>
2022-08-25 16:44:30 +0700

    membuat control, route, dan middleware product

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import ProductRoute from "./routes/ProductRoute.js";
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(ProductRoute);
 
 app.listen(process.env.APP_PORT, () => {
   console.log("Server up an running!");
